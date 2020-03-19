0daa87f3189f07c3e5d49c5400c111fd34b92c8f
Sean Lavine <sean@digit.co>
2020-03-19 10:37:12 -0700

    initial

---
+++ Formula/applesimutils@0.7.7.rb
@@ -0,0 +1,25 @@
+class ApplesimutilsAT077 < Formula
+  desc 'Apple simulator utilities'
+  homepage 'https://github.com/wix/AppleSimulatorUtils'
+  url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.7.7/AppleSimulatorUtils-0.7.7.tar.gz'
+  sha256 'b91a06a02836627ab75fd6350368f4445869453f99a2f69f5101e06521f01086'
+  head 'https://github.com/wix/AppleSimulatorUtils.git'
+
+  depends_on xcode: ['8.0', :build]
+  
+  bottle do
+    root_url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.7.7'
+    sha256 '273894479baf991291339d6ea3a68ce7f9a8dccff0c7ec3e745074ffbfdc17ff' => :catalina
+    sha256 '273894479baf991291339d6ea3a68ce7f9a8dccff0c7ec3e745074ffbfdc17ff' => :mojave
+    sha256 '273894479baf991291339d6ea3a68ce7f9a8dccff0c7ec3e745074ffbfdc17ff' => :high_sierra
+    sha256 '273894479baf991291339d6ea3a68ce7f9a8dccff0c7ec3e745074ffbfdc17ff' => :sierra
+  end
+
+  def install
+    system './buildForBrew.sh', prefix
+  end
+
+  test do
+    system "#{bin}/applesimutils", '--help'
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Hellodigit Ios-build
+
+## How do I install these formulae?
+`brew install hellodigit/ios-build/<formula>`
+
+Or `brew tap hellodigit/ios-build` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/hellodigit/homebrew-ios-build/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
