a45cfc162608566d06a6e344b192b663cfee039c
Sean Lavine <sean@digit.co>
2020-10-15 13:07:08 -0400

    add applesimutils@0.8.2

---
+++ Formula/applesimutils@0.8.2.rb
@@ -0,0 +1,17 @@
+class ApplesimutilsAT082 < Formula
+  desc 'Apple simulator utilities'
+  homepage 'https://github.com/wix/AppleSimulatorUtils'
+  url 'https://github.com/wix/AppleSimulatorUtils/releases/download/0.8.2/AppleSimulatorUtils-0.8.2.tar.gz'
+  sha256 'b616aab0e26d4a4a095b2589be130aff0d4cd215276c2d6d658a2ca99fef9caf'
+  head 'https://github.com/wix/AppleSimulatorUtils.git'
+
+  depends_on xcode: ['8.0', :build]
+  
+  def install
+    system './buildForBrew.sh', prefix
+  end
+
+  test do
+    system "#{bin}/applesimutils", '--help'
+  end
+end
